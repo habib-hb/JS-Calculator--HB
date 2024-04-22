7bb261103c0bae1edf36cc1716a44f514e6bb75b
habib-hb <developerhabib1230@gmail.com>
2024-04-22 10:36:42 +0600

    ongoing:)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [answer, setAnswer] = useState("");
+  const [expression, setExpression] = useState("");
+  const et = expression.trim();
+    const isOperator = (symbol: string) => {
+    return /[*/+-]/.test(symbol);
+    };
   const buttonPress= (symbol: string) => {
-    console.log(symbol);
+    if (symbol === "clear") {
+      setAnswer("0");
+      setExpression("");
+    } else if (symbol === "negative") {
+      if(answer === "") return;
+      setAnswer(
+        answer.charAt(0) === "-" ? answer.slice(1) : "-" + answer
+      );
+    } else if (symbol === "percentage") {
+      if(answer === "") return;
+      setAnswer(
+        (parseFloat(answer) / 100).toString()
+      );
+    } else if(isOperator(symbol)){
+        setExpression(et + " " + symbol + " ");
+    } else if(symbol === "="){
+      calculate();
+    } else if (symbol === "0"){
+      if(expression.charAt(0) !== "0"){
+        setExpression(expression + symbol);
+      }
+    }else if (symbol === "."){
+    const lastNumber = expression.split(/[*+-/]/g).pop();
+
+    if(lastNumber?.includes(".")) return;
+    setExpression(expression + symbol);
+
+    }
+
+
+  }
+
+  const calculate = () => {
+
   }
 
   return (
@@ -11,8 +51,8 @@ function App() {
         <h1>My Calculator</h1>
         <div id="calculator">
           <div id="display" style={{ textAlign: "right" }}>
-            <div id="answer"></div>
-            <div id="expression"></div>
+            <div id="answer">{answer}</div>
+            <div id="expression">{expression}</div>
           </div>
           <button id="clear" onClick={()=> buttonPress("clear")} className="light-gray">C</button>
           <button id="negative" onClick={()=> buttonPress("negative")} className="light-gray">+/-</button>
